ea92f6c62ab39fb40435fc30130099b4bf611fb1
unknown <gabriel0fontineli@gmail.com>
2021-03-07 12:30:46 -0300

    adicionando efeito

---
+++ aula14/script.js
@@ -0,0 +1,8 @@
+function toggleTable(){
+    if(document.getElementById("tabela").style.display == 'none') {
+      return document.getElementById("tabela").style.display = 'block'
+    } 
+    else {
+      return document.getElementById("tabela").style.display = 'none'
+    }
+  }
