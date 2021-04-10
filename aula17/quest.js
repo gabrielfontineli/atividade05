0204614fd58b93cfca6ee47ad6cbe5b28501ae66
unknown <gabriel0fontineli@gmail.com>
2021-04-09 23:44:36 -0300

    oi

---
+++ aula17/quest.js
@@ -0,0 +1,6 @@
+function pombadeleite(){
+    document.write('diga seu nome')
+    const nome = document.getElementById('nome').value
+    confirm("seu nome esta certo?")
+    console.write(`obrigado pela participacao ${nome} `)
+}
