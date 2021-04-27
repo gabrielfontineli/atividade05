2e4d35a99c3037c4355a1622e1d56cf8a6269ca1
BuildTools <gabriel0fontineli@gmail.com>
2021-04-27 16:54:54 -0300

    criabd ajeitado

--- avaliacao04/criabd.php
+++ avaliacao04/criabd.php
@@ -1,10 +1,16 @@
 <?php
-	
-		$sql="CREATE DATABASE db_ifrn";
+	$con = mysqli_connect("localhost", "root","");
+	if (!$con){
+		echo "Erro ao conectar com a base de dados: ".
+		mysqli_connect_error();
+	}else{
+		// Criando base de dados
+		$sql="CREATE DATABASE db_gg";
 		if (mysqli_query($con,$sql)){
 		echo "Base de Dados criada com sucesso!!";
 		}else{
 		echo "Erro: " .mysqli_error($con);
 		}
-		
+		mysqli_close($con);
+	}
 ?>
