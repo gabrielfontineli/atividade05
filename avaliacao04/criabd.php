76651d73cd68d7f2777754869b2779aff4955975
BuildTools <gabriel0fontineli@gmail.com>
2021-04-27 16:21:19 -0300

    corrigindo erro

--- avaliacao04/criabd.php
+++ avaliacao04/criabd.php
@@ -1,16 +1,10 @@
 <?php
 	
-	if (mysqli_connect_error(mysqli_connect("localhost", "root",""))){
-		echo "Erro ao conectar com a base de dados: ".
-		mysqli_connect_error();
-	}else{
-		// Criando base de dados
 		$sql="CREATE DATABASE db_ifrn";
 		if (mysqli_query($con,$sql)){
 		echo "Base de Dados criada com sucesso!!";
 		}else{
 		echo "Erro: " .mysqli_error($con);
 		}
-		mysqli_close($con);
-	}
+		
 ?>
