df1502cb42b0db3d018b17206357faa0e1a76be0
GabrielGui13 <gabrielguilherme13@hotmail.com>
2021-04-29 00:21:12 -0300

    finalizacao paginas main e inicio parcialmente

--- avaliacao04/adicionarmusica.php
+++ avaliacao04/adicionarmusica.php
@@ -2,8 +2,11 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Inserir Musica</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600;700&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/ico" href="img/logomarca-gabriel-musicas.ico"/>
     <link rel="stylesheet" href="src/musicStyle.css">
+    <title>Inserir Musica</title>
 </head>
 <body>
     <div class="container">
